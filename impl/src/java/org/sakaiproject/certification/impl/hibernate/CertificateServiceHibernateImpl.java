fa5aebd77d7838531e6bce6d40554b7db3a79c1a
Sam Ottenhoff <ottenhoff@longsight.com>
2021-03-25 14:49:53 -0400

    Updates for Sakai 21 build

--- impl/src/java/org/sakaiproject/certification/impl/hibernate/CertificateServiceHibernateImpl.java
+++ impl/src/java/org/sakaiproject/certification/impl/hibernate/CertificateServiceHibernateImpl.java
@@ -46,10 +46,10 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.orm.hibernate4.HibernateCallback;
-import org.springframework.orm.hibernate4.HibernateObjectRetrievalFailureException;
-import org.springframework.orm.hibernate4.HibernateTemplate;
-import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate5.HibernateCallback;
+import org.springframework.orm.hibernate5.HibernateObjectRetrievalFailureException;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import org.sakaiproject.antivirus.api.VirusFoundException;
 import org.sakaiproject.authz.api.AuthzGroupService;
